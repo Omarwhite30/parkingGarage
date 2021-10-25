38c8fed0c51df5418baadae31770fb1e827dd283
Omar white <omarwhite@Omars-MacBook-Pro.local>
2021-10-24 23:32:35 -0400

    bones

---
+++ main.py
@@ -0,0 +1,12 @@
+class ParkingGarage:
+    def __init__(self, tickets_sold, parking_spaces, current_tickets):
+        self.tickets_sold = tickets_sold
+        self.parking_spaces = parking_spaces
+        self.current_tickets = current_tickets
+    
+    def pay_ticket() :
+    def take_tickets() :
+    def leave_garage() :
+    def leave_garage() :
+
+
